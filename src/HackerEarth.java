de571665911dabf80169bee7fc6897c49db8315a
BABAI <BABAI@BABAI-PC>
2018-06-02 18:15:51 +0530

    First Commit

---
+++ src/HackerEarth.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author BABAI
+ *
+ */
+class HackerEarth {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This exclusively for hacker earth");
+	}
+
+}
